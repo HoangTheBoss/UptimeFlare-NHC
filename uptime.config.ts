480d10a625c03c6a068bfabc6291c8dcb4f4df9d
Nguyen Hoang <hoangtheboss.hyper@gmail.com>
2026-05-09 19:26:14 +0700

    Update notification settings in uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -124,41 +124,41 @@ const workerConfig: WorkerConfig = {
     //   timeout: 5000,
     // },
   ],
-  // // [Optional] Notification settings
-  // notification: {
-  //   // [Optional] Notification webhook settings, if not specified, no notification will be sent
-  //   // More info at Wiki: https://github.com/lyc8503/UptimeFlare/wiki/Setup-notification
-  //   webhook: {
-  //     // [Required] webhook URL (example: Telegram Bot API)
-  //     url: 'https://api.telegram.org/bot123456:ABCDEF/sendMessage',
-  //     // [Optional] HTTP method, default to 'GET' for payloadType=param, 'POST' otherwise
-  //     // method: 'POST',
-  //     // [Optional] headers to be sent
-  //     // headers: {
-  //     //   foo: 'bar',
-  //     // },
-  //     // [Required] Specify how to encode the payload
-  //     // Should be one of 'param', 'json' or 'x-www-form-urlencoded'
-  //     // 'param': append url-encoded payload to URL search parameters
-  //     // 'json': POST json payload as body, set content-type header to 'application/json'
-  //     // 'x-www-form-urlencoded': POST url-encoded payload as body, set content-type header to 'x-www-form-urlencoded'
-  //     payloadType: 'x-www-form-urlencoded',
-  //     // [Required] payload to be sent
-  //     // $MSG will be replaced with the human-readable notification message
-  //     payload: {
-  //       chat_id: 12345678,
-  //       text: '$MSG',
-  //     },
-  //     // [Optional] timeout calling this webhook, in millisecond, default to 5000
-  //     timeout: 10000,
-  //   },
-  //   // [Optional] timezone used in notification messages, default to "Etc/GMT"
-  //   timeZone: 'Asia/Shanghai',
-  //   // [Optional] grace period in minutes before sending a notification
-  //   // notification will be sent only if the monitor is down for N continuous checks after the initial failure
-  //   // if not specified, notification will be sent immediately
-  //   gracePeriod: 5,
-  // },
+  // [Optional] Notification settings
+  notification: {
+    // [Optional] Notification webhook settings, if not specified, no notification will be sent
+    // More info at Wiki: https://github.com/lyc8503/UptimeFlare/wiki/Setup-notification
+    webhook: {
+      // [Required] webhook URL (example: Telegram Bot API)
+      url: 'https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage',
+      // [Optional] HTTP method, default to 'GET' for payloadType=param, 'POST' otherwise
+      // method: 'POST',
+      // [Optional] headers to be sent
+      // headers: {
+      //   foo: 'bar',
+      // },
+      // [Required] Specify how to encode the payload
+      // Should be one of 'param', 'json' or 'x-www-form-urlencoded'
+      // 'param': append url-encoded payload to URL search parameters
+      // 'json': POST json payload as body, set content-type header to 'application/json'
+      // 'x-www-form-urlencoded': POST url-encoded payload as body, set content-type header to 'x-www-form-urlencoded'
+      payloadType: 'x-www-form-urlencoded',
+      // [Required] payload to be sent
+      // $MSG will be replaced with the human-readable notification message
+      payload: {
+        chat_id: 8204327900,
+        text: '$MSG',
+      },
+      // [Optional] timeout calling this webhook, in millisecond, default to 5000
+      timeout: 10000,
+    },
+    // [Optional] timezone used in notification messages, default to "Etc/GMT"
+    timeZone: 'Asia/Ho_Chi_Minh',
+    // [Optional] grace period in minutes before sending a notification
+    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
+    // if not specified, notification will be sent immediately
+    gracePeriod: 5,
+  },
 }
 
 // You can define multiple maintenances here
