603237bb542ae9a64f7f9245d01f34a430de0c12
Nguyen Hoang <hoangtheboss.hyper@gmail.com>
2026-06-24 01:50:10 +0700

    Modify uptime configuration for maintenance and monitors
    Updated maintenance title and added monitor ID for Minecraft. Removed Nexus Repository configuration.

--- uptime.config.ts
+++ uptime.config.ts
@@ -14,7 +14,7 @@ const pageConfig: PageConfig = {
     { link: 'mailto:me@nguyenhoang.cloud', label: 'Email', highlight: true },
   ],
   group: {
-    '🌐 nguyenhoang.cloud': ['mirrors', 'auth', 'nexusoss', 'minecraft'],
+    '🌐 nguyenhoang.cloud': ['mirrors', 'auth', 'minecraft'],
     '📷 photoworks.pw': ['pw_drive'],
   },
 }
@@ -56,28 +56,6 @@ const workerConfig: WorkerConfig = {
       target: 'https://auth.nguyenhoang.cloud/-/health/ready/',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: 'auth.nguyenhoang.cloud',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      // statusPageLink: 'https://example.com',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      headers: {
-        'User-Agent': 'Uptimeflare',
-      },
-    },
-    {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'nexusoss',
-      // `name` is used at status page and callback message
-      name: 'Nexus Repository',
-      // `method` should be a valid HTTP Method
-      method: 'HEAD',
-      // `target` is a valid URL
-      target: 'https://nexus.nguyenhoang.cloud/',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'nexus.nguyenhoang.cloud',
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
       // statusPageLink: 'https://example.com',
       // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
@@ -171,9 +149,9 @@ const workerConfig: WorkerConfig = {
 const maintenances: MaintenanceConfig[] = [
   {
     // [Optional] Monitor IDs to be affected by this maintenance
-    // monitors: [],
+    monitors: ['minecraft'],
     // [Optional] default to "Scheduled Maintenance" if not specified
-    title: 'Unscheduled Maintenance',
+    title: 'MC Server Downtime',
     // Description of the maintenance, will be shown at status page
     body: 'Hardware Failure Recovery',
     // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
@@ -182,7 +160,7 @@ const maintenances: MaintenanceConfig[] = [
     // if not specified, the maintenance will be considered as on-going
     // end: '2050-01-01T00:00:00+08:00',
     // [Optional] color of the maintenance alert at status page, default to "yellow"
-    // color: 'blue',
+    color: 'blue',
   },
 ]
 
